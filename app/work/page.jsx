ba67e93e25909da7501ad46443c783b36382ed7c
RuxueJ <rjin@sfsu.edu>
2025-01-24 13:44:10 -0800

    finish the work, need modification

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -20,6 +20,8 @@ import Link from "next/link";
 
 import Image from "next/image";
 
+import WorkSliderBtns from "@/components/WorkSliderBtns";
+
 const projects = [
   {
     num: "01",
@@ -154,7 +156,7 @@ const Work = () => {
                   <SwiperSlide key={index} className="w-full">
                     <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
                       {/* overlay */}
-                      <div></div>
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10 "></div>
 
                       {/* image */}
                       <div className="relative w-full h-full">
@@ -168,6 +170,13 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
+
+              {/*slider buttons*/}
+
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%-22px)] xl:bottom-[0] z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transiton-all "
+              />
             </Swiper>
           </div>
         </div>
